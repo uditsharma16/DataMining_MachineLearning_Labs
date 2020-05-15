771c67ef16e05da6ac09b9c821ef46ccc762052c
uditsharma16 <uditsharma16@yahoo.com>
2020-05-15 19:49:39 +1200

    Lab 5-Decision tree confusion matrix

--- Lab 5.py
+++ Lab 5.py
@@ -71,4 +71,73 @@ print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 # ...
 # ## Reading a confusion matrix
 
+from sklearn.metrics import confusion_matrix
+#
+pipe_dt.fit(X_train, y_train)
+y_pred = pipe_dt.predict(X_test)
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+#
 
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+#
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+plt.tight_layout()
+plt.savefig('06_09.png', dpi=300)
+plt.show()
+#
+#
+#
+from sklearn.metrics import precision_score, recall_score, f1_score
+#
+print('Precision: %.3f' % precision_score(y_true=y_test, y_pred=y_pred))
+print('Recall: %.3f' % recall_score(y_true=y_test, y_pred=y_pred))
+print('F1: %.3f' % f1_score(y_true=y_test, y_pred=y_pred))
+#
+
+from sklearn.metrics import make_scorer
+from sklearn.metrics import roc_curve, auc
+#from scipy import interp ##no such library
+#from scipy import interpolate
+from numpy import interp
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2),
+                        LogisticRegression(penalty='l2', solver='lbfgs', max_iter=1000,
+                                           random_state=1,C=100.0))
+X_train2 = X_train[:, [4, 14]]
+cv = list(StratifiedKFold(n_splits=3, random_state=1).split(X_train, y_train))
+fig = plt.figure(figsize=(7, 5))
+#
+mean_tpr = 0.0
+mean_fpr = np.linspace(0, 1, 100)
+all_tpr = []
+for i, (train, test) in enumerate(cv):
+    probas = pipe_lr.fit(X_train2[train], y_train[train]).predict_proba(X_train2[test])
+    fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
+    mean_tpr += interp(mean_fpr, fpr, tpr)
+    mean_tpr[0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, label='ROC fold %d (area = %0.2f)' % (i+1, roc_auc))
+#
+plt.plot([0, 1],[0, 1], linestyle='--', color=(0.6, 0.6, 0.6), label='random guessing')
+#
+mean_tpr /= len(cv)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+plt.plot(mean_fpr, mean_tpr, 'k--', label='mean ROC (area = %0.2f)' % mean_auc, lw=2)
+plt.plot([0, 0, 1], [0, 1, 1], linestyle=':', color='black', label='perfect performance')
+#
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('false positive rate')
+plt.ylabel('true positive rate')
+plt.legend(loc="lower right")
+plt.tight_layout()
+plt.savefig('06_10.png', dpi=300)
+plt.show()
